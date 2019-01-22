86ba849311678967e5e5612c21c0244b9a730e7e
eshell <eshellster@gmail.com>
2019-01-22 18:11:18 +0900

    URL 입력값 사용

---
+++ syntax/template.js
@@ -0,0 +1,14 @@
+var name = 'k8805';
+var letter =
+	'Der ' +
+	name +
+	' llskda dkjskael dkks ieae ehafh djsu djhsudfd dj djsah pad eishe di1dp zn eioqq eieqpdkdpqeidia. ' +
+	name +
+	'Ut dksjcp didenq q clzokd cirn sodd r dwid.' +
+	name;
+console.log(letter);
+
+var letter = `'Der ${name} 
+ llskda dkjskael dkks ieae ehafh djsu djhsudfd dj djsah pad eishe di1dp zn eioqq eieqpdkdpqeidia. ${name} Ut dksjcp didenq q clzokd cirn sodd r dwid.
+${name}`;
+console.log(letter);
